0051954f3f1db490f8126c3a8639c2537e53c6f1
dex <dexter.i.horthy@gmail.com>
2024-02-11 17:09:29 -0600

    try a tweak to the wf

--- marketplace/ecr_iam.tf
+++ marketplace/ecr_iam.tf
@@ -1,37 +1,58 @@
-resource "aws_iam_policy" "ecr_push_policy" {
-  name        = "ECRPushAccessForSlackerNewsMarketplace"
-  description = "Policy that allows pushing images to ECR repositories"
+# Policy for GetAuthorizationToken on all resources
+resource "aws_iam_policy" "ecr_token_policy" {
+  name        = "ECRTokenAccess"
+  description = "Allow GetAuthorizationToken on all ECR resources"
 
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "ecr:GetAuthorizationToken",
-        "ecr:BatchCheckLayerAvailability",
-        "ecr:InitiateLayerUpload",
-        "ecr:UploadLayerPart",
-        "ecr:CompleteLayerUpload",
-        "ecr:PutImage"
-      ],
-      "Resource": [
-        "arn:aws:ecr:us-east-1:709825985650:repository/slackernews/slackernews",
-        "arn:aws:ecr:us-east-1:709825985650:repository/slackernews/slackernews-chart",
-        "arn:aws:ecr:us-east-1:709825985650:repository/slackernews/slackernews-nginx"
-      ]
-    }
-  ]
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "ecr:GetAuthorizationToken"
+        Resource = "*"
+      },
+    ]
+  })
 }
-EOF
+
+# Policy for specific actions on designated repositories
+resource "aws_iam_policy" "ecr_repo_policy" {
+  name        = "ECRRepoAccessSlackerNewsMarketplace"
+  description = "Allow actions on specific ECR repositories for the SlackerNews Marketplace listing"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Action    = [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:CompleteLayerUpload",
+          "ecr:InitiateLayerUpload",
+          "ecr:PutImage",
+          "ecr:UploadLayerPart"
+        ]
+        Resource = [
+          "arn:aws:ecr:us-east-1:709825985650:repository/slackernews/slackernews",
+          "arn:aws:ecr:us-east-1:709825985650:repository/slackernews/slackernews-chart",
+          "arn:aws:ecr:us-east-1:709825985650:repository/slackernews/slackernews-nginx"
+        ]
+      },
+    ]
+  })
+}
+
+# Attach the policies to the IAM user
+resource "aws_iam_user_policy_attachment" "attach_ecr_token_policy" {
+  user       = aws_iam_user.ecr_push_user.name
+  policy_arn = aws_iam_policy.ecr_token_policy.arn
+}
+
+resource "aws_iam_user_policy_attachment" "attach_ecr_repo_policy" {
+  user       = aws_iam_user.ecr_push_user.name
+  policy_arn = aws_iam_policy.ecr_repo_policy.arn
 }
 
 resource "aws_iam_user" "ecr_push_user" {
   name = "ecr-slackernews-marketplace-push-user"
-}
-
-resource "aws_iam_user_policy_attachment" "attach_ecr_push_policy" {
-  user       = aws_iam_user.ecr_push_user.name
-  policy_arn = aws_iam_policy.ecr_push_policy.arn
 }
