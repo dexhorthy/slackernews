83960582206ef3e08b56c809614bb3d369e871fa
dex <dexter.i.horthy@gmail.com>
2024-02-18 20:16:52 -0600

    wip on auto-mpl

--- marketplace/ecr_iam.tf
+++ marketplace/ecr_iam.tf
@@ -49,6 +49,31 @@ resource "aws_iam_policy" "ecr_repo_policy" {
   })
 }
 
+resource "aws_iam_policy" "marketplace_changeset_policy" {
+  name        = "MarketplaceManagementSlackerNews"
+  description = "Allow changeset management actions on SlackerNews Marketplace listing"
+
+  # todo - this is kinda permissive, see if there's a way to restrict to specific ContainerProduct ARNs
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Action    = [
+          "aws-marketplace:ListChangeSets",
+          "aws-marketplace:DescribeEntity",
+          "aws-marketplace:StartChangeSet",
+          "aws-marketplace:ListEntities",
+          "aws-marketplace:DescribeChangeSet",
+        ]
+        Resource = [
+          "*"
+        ]
+      },
+    ]
+  })
+}
+
 # Attach the policies to the IAM user
 resource "aws_iam_user_policy_attachment" "attach_ecr_token_policy" {
   user       = aws_iam_user.ecr_push_user.name
@@ -60,9 +85,9 @@ resource "aws_iam_user_policy_attachment" "attach_ecr_repo_policy" {
   policy_arn = aws_iam_policy.ecr_repo_policy.arn
 }
 
-resource "aws_iam_user_policy_attachment" "attach_dex_ecr_policy" {
-  user       = "dex"
-  policy_arn = aws_iam_policy.ecr_repo_policy.arn
+resource "aws_iam_user_policy_attachment" "attach_marketplace_changeset_policy" {
+  user       = aws_iam_user.ecr_push_user.name
+  policy_arn = aws_iam_policy.marketplace_changeset_policy.arn
 }
 
 resource "aws_iam_user" "ecr_push_user" {
